23d443190a4167c94794c727a0320540696f6577
Vasya Petukhov <v.a.petukhov@tinkoff.ru>
2025-02-14 14:10:03 +0300

    Code review

--- backend/woof-connect-business-logic/src/main/kotlin/ru/nnsh/woof_connect/validation/ValidationChain.kt
+++ backend/woof-connect-business-logic/src/main/kotlin/ru/nnsh/woof_connect/validation/ValidationChain.kt
@@ -9,43 +9,10 @@ import ru.nnsh.woof_connect.common.WfcContext
 import ru.nnsh.woof_connect.common.WfcError.Companion.CODE_VALIDATION
 import ru.nnsh.woof_connect.common.WfcState
 import ru.nnsh.woof_connect.common.dog_profile.WfcDogId
-import ru.nnsh.woof_connect.common.dog_profile.WfcDogProfileCommand
 import ru.nnsh.woof_connect.common.dog_profile.WfcOwnerId
 import ru.nnsh.woof_connect.common.helpers.fail
-import ru.nnsh.woof_connect.operation
 
-internal fun WfcChain.validationChain() = validation {
-    operation("Регистрация собаки", WfcDogProfileCommand.CREATE) {
-        trimDogName()
-        validateDogName()
-        validateDogHasWeight()
-        validateDogHasAge()
-        validateHasUserId()
-        validateDescription()
-    }
-    operation("Удаление собаки", WfcDogProfileCommand.DELETE) {
-        validateHasUserId()
-        validateHasDogId()
-    }
-    operation("Чтение собаки", WfcDogProfileCommand.READ) {
-        validateHasDogId()
-    }
-    operation("Обновление собаки", WfcDogProfileCommand.UPDATE) {
-        trimDogName()
-        validateHasDogId()
-        validateHasUserId()
-        validateDogName()
-        validateDogHasWeight()
-        validateDogHasAge()
-        validateHasUserId()
-        validateDescription()
-    }
-    operation("Список собак", WfcDogProfileCommand.LIST_ALL) {
-        validateHasUserId()
-    }
-}
-
-private fun WfcChain.validation(
+internal fun WfcChain.validation(
     block: ICorChainDsl<WfcContext, WfcCorConfiguration>.() -> Unit
 ) = chain {
     title = "validation chain"
@@ -53,12 +20,12 @@ private fun WfcChain.validation(
     block()
 }
 
-private fun WfcChain.trimDogName() = worker {
+internal fun WfcChain.trimDogName() = worker {
     title = "Очистка имени собаки"
     doWork { dogProfileRequest = dogProfileRequest.copy(name = dogProfileRequest.name.trim()) }
 }
 
-private fun WfcChain.validateDogName() = worker {
+internal fun WfcChain.validateDogName() = worker {
     title = "Проверка наличия человекочитаемого имени и его длины"
     val regex = "^\\p{L}+$".toRegex()
     on {
@@ -70,7 +37,7 @@ private fun WfcChain.validateDogName() = worker {
     }
 }
 
-private fun WfcChain.validateDogHasWeight() = worker {
+internal fun WfcChain.validateDogHasWeight() = worker {
     title = "Проверка, что заполнен вес собаки"
     on {
         state == WfcState.RUNNING && dogProfileRequest.weight <= 0
@@ -80,7 +47,7 @@ private fun WfcChain.validateDogHasWeight() = worker {
     }
 }
 
-private fun WfcChain.validateDogHasAge() = worker {
+internal fun WfcChain.validateDogHasAge() = worker {
     title = "Проверка, что заполнен вес собаки"
     on {
         state == WfcState.RUNNING && dogProfileRequest.age < 0
@@ -90,7 +57,7 @@ private fun WfcChain.validateDogHasAge() = worker {
     }
 }
 
-private fun WfcChain.validateHasUserId() = worker {
+internal fun WfcChain.validateHasUserId() = worker {
     title = "Проверка, что указан владелец"
     on {
         state == WfcState.RUNNING && dogProfileRequest.ownerId == WfcOwnerId.None
@@ -100,7 +67,7 @@ private fun WfcChain.validateHasUserId() = worker {
     }
 }
 
-private fun WfcChain.validateHasDogId() = worker {
+internal fun WfcChain.validateHasDogId() = worker {
     title = "Проверка, что указан id собаки"
     on {
         state == WfcState.RUNNING && dogProfileRequest.dogId == WfcDogId.None
@@ -110,7 +77,7 @@ private fun WfcChain.validateHasDogId() = worker {
     }
 }
 
-private fun WfcChain.validateDescription() = worker {
+internal fun WfcChain.validateDescription() = worker {
     title = "Описание не пустое, если заполнено"
     on {
         state == WfcState.RUNNING && dogProfileRequest.description?.isBlank() == true
@@ -119,4 +86,3 @@ private fun WfcChain.validateDescription() = worker {
         fail(CODE_VALIDATION, "description", "noContent", "Description is blank")
     }
 }
-
